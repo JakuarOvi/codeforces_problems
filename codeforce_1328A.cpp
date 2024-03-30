2bde8a575a3b9a3269c9e8e51155e446949f80c7
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-03-31 03:43:21 +0600

    Add files via upload

---
+++ codeforce_1328A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    long long int a,b;
+    cin>>t;
+    while(t--)
+    {
+        int div=0,pls=0;
+        cin>>a>>b;
+        if(a%b==0){
+            cout<<0<<endl;
+            continue;
+        }
+
+        div=a/b;
+        pls=(div+1)*b;
+        cout<<pls-a<<endl;
+
+    }
+}
