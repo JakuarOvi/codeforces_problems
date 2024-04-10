12fed11b17b19036c5f350a20e65db1fc94f4fd3
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-04-10 12:20:53 +0600

    Add files via upload

---
+++ codeforce_750A.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+
+int main ()
+{
+    int n,k,i,sum,res,c;
+
+    while (cin >> n >> k)
+    {
+        res = 240 - k, sum = 0, c = 0;
+
+        for (i=1; i<=n; i++)
+        {
+            sum += 5 * i;
+
+            if (sum > res)
+                break;
+
+            c = c + 1;
+        }
+
+        cout << c << endl;
+    }
+
+    return 0;
+}
