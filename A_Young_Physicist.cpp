aaa17cca451ffbb50044ee1d3226e8d1d16c0b96
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-04-29 21:54:46 +0600

    Add files via upload

---
+++ A_Young_Physicist.cpp
@@ -0,0 +1,37 @@
+//MD Jakuar Hossain Øvi 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sort(x)        x.begin(),x.end()
+#define yes            cout<<"YES"<<endl
+#define no             cout<<"NO"<<endl
+#define ll             long long int 
+#define fl(i,a,b)      for(int i=a;i<=b;i++)
+#define fr(i,b,a)      for(int i=b;i>=a;i--)
+#define tc             int t; cin>>t; while(t--)
+#define cin(n)         int n; cin>>n;
+#define vi             vector<int>
+#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
+#define mod            1000000007
+
+int main() {
+    // Your code here
+     int n, x, y, z, xsum=0, ysum=0, zsum=0;
+    cin >> n;
+
+    while (n--)
+    {
+        cin >> x >> y >> z;
+        xsum += x;
+        ysum += y;
+        zsum += z;
+    }
+
+    if (xsum == 0 && ysum == 0 && zsum == 0)
+   yes;
+    else
+    no;
+
+    return 0;
+}
