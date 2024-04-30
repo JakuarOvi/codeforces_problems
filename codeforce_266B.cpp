5529d4ea0a4b48a7e4a75a32e21a9c8f24821393
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-04-30 17:20:11 +0600

    Create codeforce_266B.cpp

---
+++ codeforce_266B.cpp
@@ -0,0 +1,61 @@
+//MD Jakuar Hossain Øvi 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sort(x)        x.begin(),x.end()
+#define yes            cout<<"YES"<<endl
+#define no             cout<<"NO"<<endl
+#define ll             long long int 
+#define fl(i,a,b)      for(int i=a;i<=b;i++)
+#define fr(i,b,a)      for(int i=b;i>=a;i--)
+#define tc             int t; cin>>t; while(t--)
+#define cin(n)         int n; cin>>n;
+#define vi             vector<int>
+#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
+#define mod            1000000007
+
+int main() {
+    // Your code here
+      int n, t;
+    string s;
+    cin >> n >> t >> s;
+    while (t--)
+    {
+        for (int i = 1; i < n; ++i)
+        {
+            if (s[i] == 'G' && s[i-1] == 'B')
+            {
+                s[i] = 'B';
+                s[i-1] = 'G';
+                ++i;
+            }
+        }
+    }
+    cout << s << endl;
+
+    return 0;
+}
+
+/*
+input
+5 1
+BGGBG
+
+output
+GBGGB
+
+input
+5 2
+BGGBG
+
+output
+GGBGB
+
+input 
+4 1
+GGGB
+
+output 
+GGGB
+*/
