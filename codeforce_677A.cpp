23c26f657976be2a68353d06c24e1ee67bdbf11b
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-04-30 17:12:34 +0600

    Create codeforce_677A.cpp

---
+++ codeforce_677A.cpp
@@ -0,0 +1,31 @@
+//MD Jakuar Hossain Øvi 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sort(x)        x.begin(),x.end()
+#define yes            cout<<"YES"<<endl
+#define no             cout<<"NO"<<endl
+#define ll             long long int 
+#define fl(i,a,b)      for(int i=a;i<=b;i++)
+#define fr(i,b,a)      for(int i=b;i>=a;i--)
+#define tc             int t; cin>>t; while(t--)
+#define cin(n)         int n; cin>>n;
+#define vi             vector<int>
+#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
+#define mod            1000000007
+
+int main() {
+    // Your code here
+    ll n,h,ans,x;
+	cin >> n >> h;
+	ans = n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> x;
+		ans += (x>h);
+	}
+	cout << ans << endl;
+
+    return 0;
+}
