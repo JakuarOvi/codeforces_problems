d56a65556b1c28e469363cfe5bdf32d4731ae5e3
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-05-08 15:03:18 +0600

    Add files via upload

---
+++ codeforce_1296A.cpp
@@ -0,0 +1,45 @@
+//MD Jakuar Hossain Ovi
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sort(x)        sort(a,a+n)
+#define yes            cout<<"YES"<<endl
+#define no             cout<<"NO"<<endl
+#define ll             long long int 
+#define fl(i,a,b)      for(int i=a;i<=b;i++)
+#define fr(i,b,a)      for(int i=b;i>=a;i--)
+#define tc             int t; cin>>t; while(t--)
+#define cin(n)         int n; cin>>n;
+#define vi             vector<int>
+#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
+#define mod            1000000007
+int main() {
+    // Your code here
+tc
+{
+   int n;
+        cin>>n;
+        int a[n];
+        int sum = 0, odd = 0, even = 0;
+        for(int i=0; i<n; i++)
+        {
+           cin>>a[i];
+           if(a[i]%2!=0 || a[i]==1)
+           odd++;
+           else
+           even++;
+           sum += a[i];
+       }
+       if(sum%2!=0 || sum==1)
+       cout<<"YES"<<endl;
+       else
+       {
+           if(odd!=0 && even!=0)
+           cout<<"YES"<<endl;
+           else
+           cout<<"NO"<<endl;
+       }
+    }
+
+    return 0;
+}
