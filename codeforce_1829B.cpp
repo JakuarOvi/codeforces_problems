ef2b86ab267c3f81e47a9e3099718ba5bc28a271
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-05-08 14:55:16 +0600

    Add files via upload

---
+++ codeforce_1829B.cpp
@@ -0,0 +1,41 @@
+//MD Jakuar Hossain Ovi
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sort(x)        sort(a,a+n)
+#define yes            cout<<"YES"<<endl
+#define no             cout<<"NO"<<endl
+#define ll             long long int 
+#define fl(i,a,b)      for(int i=a;i<=b;i++)
+#define fr(i,b,a)      for(int i=b;i>=a;i--)
+#define tc             int t; cin>>t; while(t--)
+#define cin(n)         int n; cin>>n;
+#define vi             vector<int>
+#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
+#define mod            1000000007
+int main() {
+    // Your code here
+    tc{
+     int n;
+        cin>>n;
+        int a[n];
+        int maxi = 0;
+        int c = 0;
+        for(int i=0; i<n; i++)
+        {
+            cin>>a[i];
+            if(a[i]==0)
+            {
+                c++;
+                maxi = max(maxi,c);
+            }
+            else
+            {
+                c = 0;
+            }
+        }
+        cout<<maxi<<endl;
+    }
+
+    return 0;
+}
