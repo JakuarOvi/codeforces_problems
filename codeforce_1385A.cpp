98882384e5c6c31b606c67812ceea8b1ecc04530
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-05-08 15:26:36 +0600

    Update codeforce_1385A.cpp

--- codeforce_1385A.cpp
+++ codeforce_1385A.cpp
@@ -1,29 +1,22 @@
-//MD Jakuar Hossain Ovi
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long
 
-#define sort(x)        sort(a,a+n)
-#define yes            cout<<"YES"<<endl
-#define no             cout<<"NO"<<endl
-#define ll             long long int 
-#define fl(i,a,b)      for(int i=a;i<=b;i++)
-#define fr(i,b,a)      for(int i=b;i>=a;i--)
-#define tc             int t; cin>>t; while(t--)
-#define cin(n)         int n; cin>>n;
-#define vi             vector<int>
-#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
-#define mod            1000000007
-int main() {
-    // Your code here
-    tc
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
     {
-          int a[3];
+        int a[3];
+        
         cin>>a[0]>>a[1]>>a[2];
+ 
 		sort(a,a+3);
-		if(a[1]==a[2] || a[0]==a[2] || a[0]==a[1]) 
-         yes;
-        cout<<a[2]<<" "<<a[0]<<" "<<a[0]<<endl;
+		if(a[1]==a[2])
+         cout<<"YES"<<endl<<a[2]<<" "<<a[0]<<" "<<a[0]<<endl;
 		else 
-        no;
+        cout<<"NO"<<endl;
     }
-    }
+    return 0;
+}
