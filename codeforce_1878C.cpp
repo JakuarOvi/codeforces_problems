a56d9e6c0f5d711fb2ac2977d69cea08beda13ee
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-05-09 17:35:25 +0600

    Add files via upload

---
+++ codeforce_1878C.cpp
@@ -0,0 +1,30 @@
+//MD Jakuar Hossain Ovi
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sort(x)        sort(a,a+n)
+#define yes            cout<<"YES"<<endl
+#define no             cout<<"NO"<<endl
+#define ll             long long int 
+#define fl(i,a,b)      for(int i=a;i<=b;i++)
+#define fr(i,b,a)      for(int i=b;i>=a;i--)
+#define tc             int t; cin>>t; while(t--)
+#define cin(n)         int n; cin>>n;
+#define vi             vector<int>
+#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
+#define mod            1000000007
+int main() {
+    // Your code here
+    tc
+    {
+        ll n,k,x;
+        cin>>n>>k>>x;
+       ll max=(n*(n+1)-(n-k)*(n-k+1))/2;
+    ll min=k*(k+1)/2;
+    if(x<=max && x>=min)
+    yes;
+    else 
+    no;
+    }
+    return 0;
+}
