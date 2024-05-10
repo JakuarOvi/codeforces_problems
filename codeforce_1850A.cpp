82b1717ca50140b1822c891481ddd33a80aa3b66
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-05-11 00:05:28 +0600

    Add files via upload

---
+++ codeforce_1850A.cpp
@@ -0,0 +1,24 @@
+//MD Jakuar Hossain Ovi 
+#include<bits/stdc++.h>
+#define ll long long
+#define tc  int t; cin>>t; while(t--)
+using namespace std;
+int main()
+{
+    int a,b,c;
+    tc
+    {
+    cin>>a>>b>>c;
+    if(a+b>=10 || b+c>=10 || a+c>=10)
+    {
+    cout<<"YES"<<endl;
+    }
+    else
+    {
+    cout<<"NO"<<endl;
+    }
+        
+    }
+
+    return 0;
+}
