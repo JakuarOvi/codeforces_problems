e80ed12b4594534515b3f2d422eb09072e1da66c
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-05-11 19:39:23 +0600

    Add files via upload

---
+++ codeforce_151A.cpp
@@ -0,0 +1,20 @@
+//MD Jakuar Hossain Ovi 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define tc  int t; cin>>t; while(t--)
+#define yes   cout<<"YES"<<endl
+#define no   cout<<"NO"<<endl
+
+int main()
+{
+    
+    int n, k, l, c, d, p, nl, np;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+    cout << min(min(k * l / nl, c * d), p / np) / n << endl;
+   
+    
+
+    return 0;
+}
