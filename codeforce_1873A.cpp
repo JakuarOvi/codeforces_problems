42c6ee882e61447e2d0dc8108b9b8c33e1239df5
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-05-14 01:23:26 +0600

    Add files via upload

---
+++ codeforce_1873A.cpp
@@ -0,0 +1,26 @@
+//MD Jakuar Hossain Ovi 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define tc  int t; cin>>t; while(t--)
+#define yes   cout<<"YES"<<endl
+#define no   cout<<"NO"<<endl
+
+void solve()
+{
+    string s;
+    cin>>s;
+    if(s[0]=='a' || s[1]=='b' || s[2]=='c')
+        yes;
+    else
+        no;
+}
+ 
+int main()
+{
+    tc
+     {
+     solve();
+     }
+}
