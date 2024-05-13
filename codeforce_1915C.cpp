c384d9d6aeb6f9ddccedb6b498c389ec4cc45119
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-05-13 19:43:17 +0600

    Add files via upload

---
+++ codeforce_1915C.cpp
@@ -0,0 +1,39 @@
+//MD Jakuar Hossain Ovi
+#include<bits/stdc++.h>
+using namespace std;
+
+#define a[n]       int n; cin>>n;int a[n]
+#define sort(x)        sort(a,a+n)
+#define yes            cout<<"YES"<<endl
+#define no             cout<<"NO"<<endl
+#define ll             long long int 
+#define fl(i,a,b)      for(int i=a;i<=b;i++)
+#define fr(i,b,a)      for(int i=b;i>=a;i--)
+#define tc             int t; cin>>t; while(t--)
+#define cin(n)         int n; cin>>n;
+#define vi             vector<int>
+#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
+#define mod            1000000007
+int main() {
+    // Your code here
+    tc
+    {
+         int n;
+         cin >> n;
+
+        ll sum = 0;
+        for (int i = 0; i < n; ++i) {
+            int a;
+             cin >> a;
+            sum += a;
+        }
+
+        ll x = sqrt(sum);
+
+        if (x*x == sum) 
+           yes;
+        else no;
+    }
+
+    return 0;
+}
