2973aaf720a33dbc74f31e86be57c87b9aa02808
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-05-14 17:31:58 +0600

    Add files via upload

---
+++ codeforce_1475A.cpp
@@ -0,0 +1,26 @@
+//MD Jakuar Hossain Ovi 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define tc  int t; cin>>t; while(t--)
+#define yes   cout<<"YES"<<endl
+#define no   cout<<"NO"<<endl
+
+
+int main()
+{
+  
+      tc
+      {
+       
+        ll n;
+        cin>>n;
+        if((n&(n-1))==0)
+        no;
+        else 
+        yes;
+        
+      }
+}
+    
