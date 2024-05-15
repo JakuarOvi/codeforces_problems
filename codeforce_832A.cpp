86f48e758cc5d91b937bbdd89b084a42457cedb1
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-05-16 00:08:09 +0600

    Add files via upload

---
+++ codeforce_832A.cpp
@@ -0,0 +1,32 @@
+//MD Jakuar Hossain Ovi 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define tc  int t; cin>>t; while(t--)
+#define yes   cout<<"YES"<<endl
+#define no   cout<<"NO"<<endl
+
+
+int main()
+{
+  ll n,k,res;
+
+    while (cin >> n >> k)
+    {
+        res = n/k;
+
+        if (res & 1)
+            yes;
+        else
+            no;
+    }
+   
+  }
+      
+      
+       
+      
+}
+    
+    
