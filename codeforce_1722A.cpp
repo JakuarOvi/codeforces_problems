835525634ec4eac552a52ac29f141e405d96e54c
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-05-17 12:01:21 +0600

    Add files via upload

---
+++ codeforce_1722A.cpp
@@ -0,0 +1,33 @@
+//MD Jakuar Hossain Ovi 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define tc  int t; cin>>t; while(t--)
+#define yes   cout<<"YES"<<endl
+#define no   cout<<"NO"<<endl
+
+
+int main()
+{
+   tc
+   {
+   int n;
+   cin>>n;
+   string s,t="Timur";
+   cin>>s;
+   sort(s.begin(),s.end());
+   sort(t.begin(),t.end());
+   if(s==t) yes;
+   else no;
+   }
+  
+    
+}
+
+
+       
+      
+
+    
+    
