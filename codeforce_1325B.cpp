4795904ae0e6e5acf47378a962b8791b6954e035
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-05-22 12:51:12 +0600

    Add files via upload

---
+++ codeforce_1325B.cpp
@@ -0,0 +1,32 @@
+//MD Jakuar Hossain Øvi 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define sort(x)        x.begin(),x.end()
+#define yes            cout<<"YES"<<endl
+#define no             cout<<"NO"<<endl
+#define ll             long long int 
+#define fl(i,a,b)      for(int i=a;i<=b;i++)
+#define fr(i,b,a)      for(int i=b;i>=a;i--)
+#define tc             int t; cin>>t; while(t--)
+#define cin(n)         int n; cin>>n;
+#define vi             vector<int>
+#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
+#define mod            1000000007
+ 
+int main() {
+    // Your code here
+    tc{
+         int a,b;
+        cin>>a;
+        set<int>s;
+        while(a--)
+        {
+            int b;
+            cin>>b;
+            s.insert(b);
+        }
+        cout<<s.size()<<endl;
+     }
+  }
