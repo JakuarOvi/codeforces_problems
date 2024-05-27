78d8b2c9c7e7cbc56c17b0269ad6ea8593852d6d
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-05-28 00:13:15 +0600

    Add files via upload

---
+++ codeforce_1283A.cpp
@@ -0,0 +1,29 @@
+//MD Jakuar Hossain Ovi
+#include<bits/stdc++.h>
+using namespace std;
+#define sort(a)        sort(a,a+n)
+#define yes            cout<<"YES"<<endl
+#define no             cout<<"NO"<<endl
+#define ll             long long int 
+#define fl(i,a,b)      for(int i=a;i<=b;i++)
+#define fr(i,b,a)      for(int i=b;i>=a;i--)
+#define tc             int t; cin>>t; while(t--)
+#define cin(n)         int n; cin>>n;
+#define vi             vector<int>
+#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
+#define mod            1000000007
+
+int main() {
+    tc
+    {
+    int a,b;
+    cin>>a>>b;
+    int minute=(a*60)+b;
+    int result=(24*60)-minute;
+    cout<<result<<endl;
+    }
+
+
+    return 0;
+}
+    
