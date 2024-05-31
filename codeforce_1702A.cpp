52309fdff90c635fed969edd1e65409b5e6932c4
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-05-31 11:42:14 +0600

    Add files via upload

---
+++ codeforce_1702A.cpp
@@ -0,0 +1,31 @@
+//MD Jakuar Hossain Ovi
+#include<bits/stdc++.h>
+using namespace std;
+#define sort(a)        sort(a,a+n)
+#define yes            cout<<"YES"<<endl
+#define no             cout<<"NO"<<endl
+#define ll             long long int 
+#define cin(n)         int n; cin>>n;
+#define tc             int t; cin>>t; while(t--)
+#define vi             vector<int>
+#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
+#define loop(a,n)     for(int i=0;i<n;i++)
+#define mod            1000000007
+#define end1           "\n"
+
+
+int main() {
+    
+    tc{
+    ll n;
+    cin>>n;
+    ll x=1;
+    while(x*10<=n)
+    {
+    x*=10;
+    }
+    cout<<n-x<<endl;
+    }
+
+    return 0;
+}
