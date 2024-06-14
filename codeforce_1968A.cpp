64b969820fe574248574e3fd78a0503a01c9b8b9
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-06-14 14:49:23 +0600

    Create codeforce_1968A.cpp

---
+++ codeforce_1968A.cpp
@@ -0,0 +1,30 @@
+//Bismillahir Rahmanir Rahim
+//MD Jakuar Hossain Ovi
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sort(a)        sort(a,a+n)
+#define yes            cout<<"YES"<<endl
+#define no             cout<<"NO"<<endl
+#define ll             long long int 
+#define fl(i,a,b)      for(int i=a;i<=b;i++)
+#define fr(i,b,a)      for(int i=b;i>=a;i--)
+#define tc             int t; cin>>t; while(t--)
+#define cin(n)         int n; cin>>n;
+#define vi             vector<int>
+#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
+#define mod            1000000007
+#define endl          "\n" 
+#define FastRead       ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+int main() {
+FastRead;
+    //  write Your code here
+    tc
+    {
+        int n;
+        cin>>n;
+        cout<<n-1<<endl;
+    }
+
+    return 0;
+}
